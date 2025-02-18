89e8cf3c76eb7524b1bea987352a43ea396c7805
snacktavish <ejmctavish@gmail.com>
2025-02-17 17:44:11 -0800

    documentation in example script

--- R/getCitations.R
+++ R/getCitations.R
@@ -81,7 +81,7 @@ getCitations <- function(tree, version="1.3", data_path=FALSE)
   
   if (data_path == ""){
     data(clootl_data)
-    all_nodes <- clootl_data$trees$`Aves_1.3`$annotations
+    all_nodes <- clootl_data$trees$`Aves_1.3`$annotations ##ToDO not hardcode?
   } else{
       filename <- paste(data_path, '/Tree_versions/Aves_', version, '/OpenTreeSynth/annotated_supertree/annotations.json', sep='')
       if (!file.exists(filename)){    
--- example/AvesDataDemo.R
+++ example/AvesDataDemo.R
@@ -10,24 +10,59 @@ library(ape)
 setwd("~/projects/otapi/OpenTreeCLO/clootlDemo")
 
 
+##You should be able to get default trees (currently v 1.3) without downloading the data. 
+ex1 <- extractTree(species=c("Turdus migratorius",
+                             "Setophaga dominica",
+                             "Setophaga ruticilla",
+                             "Sitta canadensis"))
+
+ex2 <- extractTree(species=c("amerob",
+                             "canwar",
+                             "reevir1",
+                             "yerwar",
+                             "gockin"), 
+                              label_type="code")
+
+#Defualts to most recent (2023) taxonomy, but 2021, 2022, and 2023 taxonomies are available
+ex3 <- extractTree(species=c("amerob",
+                             "canwar",
+                             "reevir1",
+                             "yerwar",
+                             "gockin"), 
+                   taxonomy_year = 2021,
+                   label_type="code")
+
+
+## Pulls the citations
+getCitations(ex3) 
+
+
+## IF you have downloaded the Aves Data Repo to your computer,
+## you can pass in the path to get other tree versions
+e.g.
+#ex3 <- extractTree(species=c("Turdus migratorius","Setophaga dominica", "Setophaga ruticilla", "Sitta canadensis"),
+#                                       label_type="scientific", 
+#                               version=1.2,
+#                               data_path="~/projects/otapi/AvesData")
+
+
+
+##To get other tree versions, and or the 100 tree dated sample sets, 
 # this downloads all the data from the AvesData github repo
 # As a folder in your current working directory
-clootl:::get_avesdata_repo()
+clootl:::get_avesdata_repo() ## Currently I don't export this function correctly... gotta fix that
 
-## If you want it to re-download the git repo (to get new data) use:
+## If you want it to re-download the git repo (e.g. to get new data) use:
 # get_avesdata_repo(refresh = TRUE)
 
 
-##or if you have the AvesData repo somewhere else and don't feel like typing it in
-# set_avesdata_repo_path(PATH)
+##or if you have the AvesData repo somewhere else and don't feel like typing it in each time
+# clootl:::set_avesdata_repo_path(PATH)
 
 
 
-ex1 <- extractTree(species=c("amerob", "canwar", "reevir1", "yerwar", "gockin"),
+ex4 <- extractTree(species=c("amerob", "canwar", "reevir1", "yerwar", "gockin"),
                    label_type="code", taxonomy_year="2021", version="1.2")
 
-
-ex2 <- extractTree(species=c("Turdus migratorius", "Setophaga dominica", "Setophaga ruticilla", "Sitta canadensis"),
-                   +                    label_type="scientific", data_path="~/projects/otapi/AvesData")
-
-getCitations(ex1, version="1.2", data_path="~/projects/otapi/AvesData")
+## To pull citations for older tree versions you need to include the version number
+getCitations(ex4, version="1.2")
