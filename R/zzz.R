67a1de15879c88ce7519165a0164d28078fd5c29
snacktavish <ejmctavish@gmail.com>
2025-03-12 10:03:54 -0700

    Tree version

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,7 @@
     requireNamespace("utils")
     cit<-citation(pkgname)
     txt<-paste(c(format(cit,"citation")),collapse="\n\n")
+    txt<-paste(c(txt, "The current version of the Aves tree is v1.3.
+        Please specify the tree and taxonomy version used when using this tree."),collapse="\n\n")
     packageStartupMessage(txt)
 }
