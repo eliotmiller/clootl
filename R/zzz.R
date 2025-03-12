a7c87806f2ddc1af3a928814d581b29610d31d3c
Luna Sare <sanchez.reyes.luna@gmail.com>
2025-03-12 15:27:47 -0700

    fix warnings related to citation

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach<-function(libname, pkgname){
     requireNamespace("utils")
-    cit<-citation(pkgname)
+    cit<-utils::citation(pkgname)
     txt<-paste(c(format(cit,"citation")),collapse="\n\n")
     txt<-paste(c(txt, "The current version of the Aves tree is v1.3.
         Please specify the tree and taxonomy version used when using this tree.
