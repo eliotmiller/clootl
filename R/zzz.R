ce93da38b91e9797ba895f997f40ba88257f282f
snacktavish <ejmctavish@gmail.com>
2025-03-12 11:18:05 -0700

    Increment version

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,8 @@
     cit<-citation(pkgname)
     txt<-paste(c(format(cit,"citation")),collapse="\n\n")
     txt<-paste(c(txt, "The current version of the Aves tree is v1.3.
-        Please specify the tree and taxonomy version used when using this tree."),collapse="\n\n")
+        Please specify the tree and taxonomy version used when using this tree.
+        When possible, cite all the studies supporting your subtree:
+        These citations are acessible using getCitations(yourtree)"),collapse="\n\n")
     packageStartupMessage(txt)
 }
