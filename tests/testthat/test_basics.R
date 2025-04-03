750ba93ce41f63a9b6088f3182e731681d564e92
snacktavish <ejmctavish@gmail.com>
2025-04-03 09:20:05 -0700

    more tests

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -8,14 +8,32 @@ test_that(desc = "sci name tree extract", code = {
                       "Sitta canadensis")
 
     sci_name_tree <- extractTree(species=sci_name_spp)
-    cites <- getCitations(sci_name_tree)
-
         expect_equal( length(sci_name_tree), 6 )
-        expect_equal( length(cites), 4 )
-        expect_equal( length(cites[['study']]), 13 )
         expect_type( object = sci_name_tree, type = "list")  
         expect_equal( sci_name_tree$Nnode, 3 )
         expect_contains(sci_name_tree[['tip.label']], gsub(" ", "_",sci_name_spp))
+
+    sci_name_tree14_2021 <- extractTree(species=sci_name_spp, taxonomy_year=2021)
+    sci_name_tree14_2022 <- extractTree(species=sci_name_spp, taxonomy_year=2022)
+    sci_name_tree14_2023 <- extractTree(species=sci_name_spp, version=1.4, taxonomy_year=2023)
+
+        expect_identical(sci_name_tree, sci_name_tree14_2023)
+        expect_equal( length(sci_name_tree14_2021), 6 )
+        expect_contains(sci_name_tree14_2021[['tip.label']], gsub(" ", "_",sci_name_spp))
+        expect_equal( length(sci_name_tree14_2022), 6 )
+        expect_contains(sci_name_tree14_2022[['tip.label']], gsub(" ", "_",sci_name_spp))
+        
+    cites <- getCitations(sci_name_tree)
+
+        expect_equal( length(cites), 4 )
+        expect_equal( length(cites[['study']]), 13 )
+
+    cites2021 <- getCitations(sci_name_tree14_2021)
+    cites2022 <- getCitations(sci_name_tree14_2022)
+    cites2023 <- getCitations(sci_name_tree14_2023)
+    expect_identical(cites, cites2021)
+    expect_identical(cites, cites2022)
+    expect_identical(cites, cites2023)
 
 })
 
@@ -28,4 +46,5 @@ code_tree <- extractTree(species=code_spp,
 
 expect_identical(sort(code_tree[['tip.label']]), sort(code_spp))
 })
+
 
