c09e06933a5b2fb574564f0f82c7fe78a73fa6c0
Luna Sare <sanchez.reyes.luna@gmail.com>
2025-10-27 11:42:14 -0700

    fix local tests

--- tests/testthat/test_basics.R
+++ tests/testthat/test_basics.R
@@ -15,7 +15,7 @@ test_that(desc = "sci name tree extract", code = {
 
     sci_name_tree14_2021 <- extractTree(species=sci_name_spp, taxonomy_year=2021)
     sci_name_tree14_2022 <- extractTree(species=sci_name_spp, taxonomy_year=2022)
-    #sci_name_tree13_2023 <- extractTree(species=sci_name_spp, version=1.4, taxonomy_year=2023)
+    #sci_name_tree14_2023 <- extractTree(species=sci_name_spp, version=1.4, taxonomy_year=2023)
     sci_name_tree15_2023 <- extractTree(species=sci_name_spp, version=1.5, taxonomy_year=2023)
 
         # expect_identical(sci_name_tree, sci_name_tree14_2023)
@@ -31,7 +31,7 @@ test_that(desc = "sci name tree extract", code = {
 
     cites2021 <- getCitations(sci_name_tree14_2021)
     cites2022 <- getCitations(sci_name_tree14_2022)
-    cites2023 <- getCitations(sci_name_tree14_2023)
+    # cites2023 <- getCitations(sci_name_tree14_2023)
     expect_identical(cites, cites2021)
     expect_identical(cites, cites2022)
     # expect_identical(cites, cites2023)
