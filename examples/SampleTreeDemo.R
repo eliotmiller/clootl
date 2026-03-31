f1f0780c03f43f4451cd695dd1f6866a9a688712
snacktavish <ejmctavish@gmail.com>
2026-03-31 15:04:49 -0700

    add GENUS column

--- examples/SampleTreeDemo.R
+++ examples/SampleTreeDemo.R
@@ -16,9 +16,11 @@ ex1 <- sampleTrees(species=c("Turdus migratorius",
 densiTree(ex1)
 
 ## For the genus Columba some species were placed taxonomically
-## This results in varation in ages and relationships
-
+## This results in variation in ages and relationships
+library(stringr)
 tax <- clootl_data$taxonomies$year2025
+genus<-str_split_i(tax$SCI_NAME, " ",1)
+tax<-cbind(tax, GENUS=genus)
 columba <- tax[tax$GENUS=="Columba",]
 columba_spp <- columba$SCI_NAME
 ex2 <- sampleTrees(species=columba_spp)
