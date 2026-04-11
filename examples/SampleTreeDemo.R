d9dde19f4846e8bc467e02d945de366696eb2e55
snacktavish <ejmctavish@gmail.com>
2026-04-11 09:23:09 -0700

    update example to a monphyletic genus - Mergus

--- examples/SampleTreeDemo.R
+++ examples/SampleTreeDemo.R
@@ -15,13 +15,13 @@ ex1 <- sampleTrees(species=c("Turdus migratorius",
 ## Here there is only variation in node ages
 densiTree(ex1)
 
-## For the genus Columba some species were placed taxonomically
+## For the genus Mergus some species were placed taxonomically
 ## This results in variation in ages and relationships
 library(stringr)
 tax <- clootl_data$taxonomies$year2025
 genus<-str_split_i(tax$SCI_NAME, " ",1)
 tax<-cbind(tax, GENUS=genus)
-columba <- tax[tax$GENUS=="Columba",]
-columba_spp <- columba$SCI_NAME
-ex2 <- sampleTrees(species=columba_spp)
+Mergus <- tax[tax$GENUS=="Mergus",]
+Mergus_spp <- Mergus$SCI_NAME
+ex2 <- sampleTrees(species=Mergus_spp)
 densiTree(ex2)
